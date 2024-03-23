ebfe264e688f9686ba0b2b4cd5d7cccf464ceae0
Ondřej März <marzondr@fit.cvut.cz>
2024-03-23 10:45:10 +0100

    Renamed routes

--- app/src/main/java/cz/ondrejmarz/taborak/Destinations.kt
+++ app/src/main/java/cz/ondrejmarz/taborak/Destinations.kt
@@ -7,44 +7,42 @@ import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Groups
 import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
-import cz.ondrejmarz.taborak.ui.screens.*
 
 interface AppDestination {
     val icon: ImageVector
     val route: String
 }
 
-object Overview : AppDestination {
+object Home : AppDestination {
     override val icon = Icons.Filled.FilterList
-    override val route = "overview"
+    override val route = "Rozcestník"
 }
 
 object Tour : AppDestination {
     override val icon = Icons.Filled.People
-    override val route = "tour"
+    override val route = "Turnus"
 }
 
 object Participants : AppDestination {
     override val icon = Icons.Filled.Groups
-    override val route = "participants"
+    override val route = "Oddíl"
 }
 
-object Schedule : AppDestination {
+object Calendar : AppDestination {
     override val icon = Icons.Filled.CalendarToday
-    override val route = "schedule"
+    override val route = "Kalendář"
 }
 
 object Tasks : AppDestination {
     override val icon = Icons.Filled.Checklist
-    override val route = "tasks"
+    override val route = "Úkoly"
 }
 
 object Settings : AppDestination {
     override val icon = Icons.Filled.Settings
-    override val route = "settings"
+    override val route = "Nastavení"
 }
 
 // Screens to be displayed in the bottom tab row
-val appTabRowScreens = listOf(Tour, Participants, Schedule, Tasks, Settings)
+val appTabRowScreens = listOf(Tour, Participants, Calendar, Tasks, Settings)
